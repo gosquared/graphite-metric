f616c651ca69fd1a75713637de2ff2699f06e783
Gerhard Lazu <gerhard@lazu.co.uk>
2012-05-25 12:45:00 +0100

    These methods are specific to Plaintext, not generic utils

--- lib/graphite-metric/plaintext.rb
+++ lib/graphite-metric/plaintext.rb
@@ -1,9 +1,5 @@
-require 'graphite-metric/util'
-
 module GraphiteMetric
   Plaintext = Struct.new(:key, :value, :timestamp) do
-    extend Util
-
     def initialize(*args)
       super
       self[:timestamp] ||= Time.now.utc.to_i
@@ -11,6 +7,18 @@ module GraphiteMetric
 
     def to_s
       [key, value, timestamp].join(" ")
+    end
+
+    def self.from_hash(hash)
+      metric = new
+      members.each { |member| metric[member] = hash[member] if hash.has_key?(member) }
+      metric
+    end
+
+    def self.from_array(array)
+      array.inject([]) do |result, hash|
+        result << from_hash(hash)
+      end
     end
   end
 end
--- test/graphite-metric/plaintext_test.rb
+++ test/graphite-metric/plaintext_test.rb
@@ -12,5 +12,56 @@ module GraphiteMetric
     it "uses the graphite plaintext format when converted to string" do
       "#{Plaintext.new("visitors", 2)}".must_equal "visitors 2 #{utc_now}"
     end
+
+    before do
+      @timestamp = Time.now.to_i - 3600
+    end
+
+    describe "from hash" do
+      before do
+        @hash = {
+          :key       => "visitors",
+          :value     => 2,
+          :timestamp => @timestamp
+        }
+      end
+
+      it "can be built from a hash" do
+        "#{Plaintext.from_hash(@hash)}".must_equal "visitors 2 #{@timestamp}"
+      end
+    end
+
+    describe "from array (of hashes)" do
+      before do
+        @array = [
+          {
+            :key       => "visitors",
+            :value     => 1,
+            :timestamp => @timestamp
+          },
+          {
+            :key   => "visitors",
+            :value => 5
+          }
+        ]
+        @graphite_metrics = Plaintext.from_array(@array)
+      end
+
+      it "returns an array" do
+        @graphite_metrics.must_be_instance_of Array
+        @graphite_metrics.size.must_equal 2
+      end
+
+      it "each element is a GraphiteMetric::Plaintext" do
+        @graphite_metrics.each { |graphite_metric| graphite_metric.must_be_instance_of GraphiteMetric::Plaintext }
+      end
+
+      it "converts into graphite plaintext format" do
+        @graphite_metrics.map(&:to_s).must_equal([
+          "visitors 1 #{@timestamp}",
+          "visitors 5 #{utc_now}"
+        ])
+      end
+    end
   end
 end
